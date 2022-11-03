cbaba77cd77f7a840d013df3985d53063fcfdd05
Geanina <gea.nina1166@gmail.com>
2022-11-03 19:05:30 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- userextend/models.py
+++ userextend/models.py
@@ -9,8 +9,8 @@ class UserExtend(User):
 
     email_confirmation = models.EmailField(max_length=30)
     phone = models.CharField(max_length=10)
-    birthday = models.DateField()
-    gender = models.CharField(choices=gender_options, max_length=6)
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.CharField(choices=gender_options, max_length=6, null=True)
     account = models.CharField(choices=account_options, max_length=6, null=True)
 
 
