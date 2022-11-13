44d77dd472b8a6bec2c3a0e72ec37b1c97ffd64e
Geanina <gea.nina1166@gmail.com>
2022-11-13 19:41:03 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- artobjects/urls.py
+++ artobjects/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import ArtObjectsView, ArtObjectsDetailView, CreateArtObjectsView
+from .views import ArtObjectsView, ArtObjectsDetailView, CreateArtObjectsView, ArtObjectsUpdateView
 
 urlpatterns = [
     # path('', ArtObjectsView.as_view(), name='artobjects_list'),
     path('artobjects-list/', views.ArtObjectsView.as_view(), name='artobjects_list'),
     path('<int:pk>', ArtObjectsView.as_view(), name='artobjects_detail'),
     path('', CreateArtObjectsView.as_view(), name='create_object'),
-    path('', ArtObjectsDetailView.as_view(), name='update_objects')
+    path('', ArtObjectsUpdateView.as_view(), name='update_objects')
 ]
