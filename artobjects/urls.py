bf178bb797f5ea5a49ba80b8a890b46f86d6d0f5
Geanina <gea.nina1166@gmail.com>
2022-11-13 23:12:22 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- artobjects/urls.py
+++ artobjects/urls.py
@@ -5,8 +5,8 @@ from .views import ArtObjectsView, ArtObjectsDetailView, CreateArtObjectsView, A
 
 urlpatterns = [
     path('artobjects-list/', views.ArtObjectsView.as_view(), name='artobjects_list'),
-    path('', CreateArtObjectsView.as_view(), name='create_object'),
-    path('', ArtObjectsUpdateView.as_view(), name='update_objects'),
-    path('', ArtObjectsDetailView.as_view(), name='object_details'),
-    path('', views.ObjectDeleteView.as_view(), name='delete_object'),
+    path('create-object/', views.CreateArtObjectsView.as_view(), name='create_object'),
+    path('update-object/<int:pk>/', views.ArtObjectsUpdateView.as_view(), name='update_objects'),
+    path('object-details/<int:pk>/', views.ArtObjectsDetailView.as_view(), name='object_details'),
+    path('delete-object/<int:pk>/', views.ObjectDeleteView.as_view(), name='delete_object'),
 ]
--- artobjects/views.py
+++ artobjects/views.py
@@ -8,28 +8,28 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 
 
 class ArtObjectsView(ListView):
+    template_name = 'artobjects/artobjects_list.html'
     model = ArtObjects
     form_class = ArtObjectsForm
     context_object_name = 'artobjects_list'
-    template_name = 'artobjects/artobjects_list.html'
 
 
 class CreateArtObjectsView(CreateView):
+    template_name = 'artobjects/create_object.html'
     model = ArtObjects
     fields = '__all__'
     context_object_name = 'artobjects.product_name'
-    template_name = 'artobjects/create_object.html'
     success_url = reverse_lazy('artobjects_list')
 
 
 class ArtObjectsDetailView(DetailView):
+    template_name = 'artobjects/object_details.html'
     model = ArtObjects
     context_object_name = 'artobjects.description'
-    template_name = 'artobjects/object_details.html'
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        all_details_artobjects = ArtObjects.objects.filter(artobjects_id=self.kwargs['pk'])
+        all_details_artobjects = ArtObjects.objects.filter(product_name=self.kwargs['pk'])
         data['all_details_artobjects'] = all_details_artobjects
 
         return data
@@ -41,6 +41,11 @@ class ArtObjectsUpdateView(UpdateView):
     form_class = ArtObjectsForm
     success_url = reverse_lazy('artobjects_list')
 
+
+class ObjectDeleteView(DeleteView):
+    template_name = 'artobjects/delete_object.html'
+    model = ArtObjects
+    success_url = reverse_lazy('artobjects_list')
 
 class ObjectDeleteView(DeleteView):
     template_name = 'artobjects/delete_object.html'
