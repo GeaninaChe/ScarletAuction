915e9efd73d290e6186dd0dbbad56aafbdb1602b
Geanina <gea.nina1166@gmail.com>
2022-11-13 21:28:16 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from home import views
 
 urlpatterns = [
     path('', views.HomeTemplateView.as_view(), name="homepage"),
+    path('', views.IntroductionView.as_view(), name="our_story"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class HomeTemplateView(TemplateView):
     template_name = "home/homepage.html"
+
+
+class IntroductionView(TemplateView):
+    template_name = "home/../templates/our_story.html"
+
